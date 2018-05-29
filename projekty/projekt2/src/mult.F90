c59b18f5700e0ae590cf067fac85707c66412198
jplewa <juliadplewa@gmail.com>
2018-05-29 18:36:29 +0200

    project structure

---
+++ projekty/projekt2/src/mult.F90
@@ -0,0 +1,74 @@
+module mult
+
+    implicit none
+
+contains
+
+    subroutine mm1(m1, m2, result, error)
+
+        real (kind = 8), intent(in) :: m1(:, :), m2(:, :)
+        real (kind = 8), intent(out) :: result(:, :)
+        integer (kind = 4), intent(out) :: error
+        integer :: i, j, k, shape1(2), shape2(2), shape3(2)
+
+        result = 0.d0
+        error = 0
+
+        shape1 = shape(m1)
+        shape2 = shape(m2)
+        shape3 = shape(result)
+
+        if (shape1(1) .NE. shape2(2)) then
+            error = 1
+            return
+        endif
+        if ((shape3(1) .NE. shape2(1)) .OR. (shape3(2) .NE. shape1(2))) then
+            error = 2
+            return
+        endif
+
+        do j = 1, shape3(2)
+            do k = 1, shape1(1)
+                do i = 1, shape3(1)
+                    result(i,j) = result(i,j) + m1(k,j) * m2(i,k)
+                end do
+            end do  
+        end do
+
+    end subroutine
+
+    subroutine mm2(m1, m2, result, error)
+
+        real (kind = 8), intent(in) :: m1(:, :), m2(:, :)
+        real (kind = 8), intent(out) :: result(:, :)
+        integer (kind = 4), intent(out) :: error
+        integer :: i, j, jj, shape1(2), shape2(2), shape3(2), ichunk
+
+        result = 0.d0
+        error = 0
+
+        shape1 = shape(m1)
+        shape2 = shape(m2)
+        shape3 = shape(result)
+
+        if (shape1(1) .NE. shape2(2)) then
+            error = 1
+            return
+        endif
+        if ((shape3(1) .NE. shape2(1)) .OR. (shape3(2) .NE. shape1(2))) then
+            error = 2
+            return
+        endif
+
+        ichunk = 2048 ! I have a 3MB cache size (real*4)
+        do jj = 1, shape3(2), ichunk
+            do j = jj, min(jj + ichunk - 1, shape3(2))
+                do i = 1, shape3(1)
+                    result(i,j) = DOT_PRODUCT(m1(:,j), m2(i,:))
+                end do
+           end do
+        end do 
+    
+    end subroutine
+
+end module
