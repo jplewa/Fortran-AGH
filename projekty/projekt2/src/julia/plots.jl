1479f4100f079ababfa30e2ea5664bd373f3e3a8
jplewa <juliadplewa@gmail.com>
2018-06-01 14:52:34 +0200

    plot formatting

--- projekty/projekt2/src/julia/plots.jl
+++ projekty/projekt2/src/julia/plots.jl
@@ -6,23 +6,24 @@ function draw_plots()
     N = map(x -> parse(Int64,x), readlines(open("./out/results/N")))
     df = DataFrame(N = N,
         time = map(x -> Float64(parse(Int64,x))/ticks, readlines(open("./out/results/mm1"))),
-        mtype="mm1")
+        mtype="naive")
     df = vcat(df, DataFrame(N = N,
         time = map(x -> Float64(parse(Int64,x))/ticks, readlines(open("./out/results/mm2"))),
-        mtype="mm2"))
+        mtype="dot"))
     df = vcat(df, DataFrame(N = N,
         time = map(x -> Float64(parse(Int64,x))/ticks, readlines(open("./out/results/mm3"))),
-        mtype="mm3"))
+        mtype="ichunk"))
     df = vcat(df, DataFrame(N = N,
         time = map(x -> Float64(parse(Int64,x))/ticks, readlines(open("./out/results/mm4"))),
-        mtype="mm4"))
+        mtype="ichunk+dot"))
     df = vcat(df, DataFrame(N = N,
         time = map(x -> Float64(parse(Int64,x))/ticks, readlines(open("./out/results/matmul"))),
         mtype="matmul"))
     Gadfly.push_theme(:dark)
-    set_default_plot_size(30cm, 30cm)
-    draw(PNG("./out/plots/plot.png", 30cm, 30cm), plot(df, x="N", y="time", color="mtype", Geom.point,
-            style(major_label_font_size=14pt, minor_label_font_size=12pt),
+    #set_default_plot_size(30cm, 30cm)
+    draw(PNG("./out/plots/plot.png", 25cm, 25cm, dpi=300), plot(df, x="N", y="time", color="mtype", Geom.point,
+            style(major_label_font_size=16pt, minor_label_font_size=16pt, 
+            point_label_font_size=16pt, key_title_font_size=16pt, point_size=8pt),
             Scale.x_log2,Scale.y_log2))
 end
 
