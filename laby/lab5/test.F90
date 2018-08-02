879bd2de3a5bcda7cdceba0599b50c52b184674c
jplewa <juliadplewa@gmail.com>
2018-08-02 13:45:17 +0200

    project structure cleanup

--- laby/lab5/test.F90
+++
@@ -1,7 +0,0 @@
-program test
-   
-   implicit none
-   
-   write(*,*) this_image(), num_images()
-   
-end program
--- laby/test/test4/main.f90
+++
@@ -1,6 +0,0 @@
-program main
-  implicit none
-  integer re_i
-  write(*,*) "Hello World!"
-  re_i = system("pause")
-end
